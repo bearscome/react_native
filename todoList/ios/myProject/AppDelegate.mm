3f732fa01c5409bfed27e7f579b5083861ca626d
hyosung <67565847+bearscome@users.noreply.github.com>
2022-09-06 01:00:12 +0900

    iso 설정 변경
    myProject to todoList

--- todoList/ios/myProject/AppDelegate.mm
+++ todoList/ios/myProject/AppDelegate.mm
@@ -41,7 +41,7 @@
   bridge.surfacePresenter = _bridgeAdapter.surfacePresenter;
 #endif
 
-  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"myProject", nil);
+  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"todoList", nil);
 
   if (@available(iOS 13.0, *)) {
     rootView.backgroundColor = [UIColor systemBackgroundColor];
